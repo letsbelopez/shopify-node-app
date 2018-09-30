a3f85b90ee43b5a1be8d1b904b54a2b107f1882f
letsbelopez <letsbelopez@gmail.com>
2018-09-30 11:49:10 -0700

    getting product metafields

--- client/components/MealsPage.js
+++ client/components/MealsPage.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import { Page, Layout } from '@shopify/polaris';
-import queryString from 'query-string';
-
+import React, { Component } from "react";
+import { Page, Layout } from "@shopify/polaris";
+import queryString from "query-string";
 
 class MealsPage extends Component {
   state = {
     mealId: null
-  }
+  };
 
   componentDidMount() {
-    const query = queryString.parse(this.props.location.search)
-      this.setState( {mealId: query.Id } );
+    const query = queryString.parse(this.props.location.search);
+    this.setState({ mealId: query.Id });
+
+    const fetchOptions = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      credentials: "include"
+    };
+
+    fetch(`/shopify/api/products/${query.id}/metafields.json`, fetchOptions)
+      .then(response => response.json())
+      .then(json => console.log(json));
   }
 
   render() {
     return (
-      <Page title="Meals" >
+      <Page title="Meals">
         <Layout>
           <Layout.Section>
             <h2>Meals page welcome</h2>
